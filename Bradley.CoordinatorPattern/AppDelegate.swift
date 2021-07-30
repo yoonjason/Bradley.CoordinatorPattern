b6daa8d0aa164a6994a49c0233a5f934c75e51aa
Bradley.yoon <luffy1021@naver.com>
2021-07-30 09:49:17 +0900

    * initial commit   - 앱 초기화 세팅

---
+++ Bradley.CoordinatorPattern/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Bradley.CoordinatorPattern
+//
+//  Created by Bradley.yoon on 2021/07/30.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Bradley.CoordinatorPatternTests/Bradley_CoordinatorPatternTests.swift
@@ -0,0 +1,33 @@
+//
+//  Bradley_CoordinatorPatternTests.swift
+//  Bradley.CoordinatorPatternTests
+//
+//  Created by Bradley.yoon on 2021/07/30.
+//
+
+import XCTest
+@testable import Bradley_CoordinatorPattern
+
+class Bradley_CoordinatorPatternTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
